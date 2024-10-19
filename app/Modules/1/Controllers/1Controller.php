865a3948a199178d6406916829c6d15cafe8bd82
lqtuan123 <lqtuan0102@gmail.com>
2024-10-19 15:05:15 +0700

    #28 done

---
+++ app/Modules/1/Controllers/1Controller.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Modules\1\Controllers;
+
+use App\Http\Controllers\Controller;
+
+class 1Controller extends Controller
+{
+    public function index()
+    {
+        return view('1::index');
+    }
+}
