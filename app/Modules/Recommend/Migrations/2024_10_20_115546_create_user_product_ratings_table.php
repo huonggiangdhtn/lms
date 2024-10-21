16dc0a451cf98084e806770b9e8c9730c8c57f9c
Vu Tru <ngoquangy2002@gmail.com>
2024-10-21 12:56:02 +0700

    #39 y - create_module_recommend

--- app/Modules/Recommend/Migrations/2024_10_20_115546_create_user_product_ratings_table.php
+++ app/Modules/Recommend/Migrations/2024_10_20_115546_create_user_product_ratings_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('user_product_ratings', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('product_id')->constrained()->onDelete('cascade');
+            $table->integer('rating');
             $table->timestamps();
         });
     }
