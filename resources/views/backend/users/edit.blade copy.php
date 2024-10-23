baddf7cdcaff15847e310ea660b4662da63725b0
lqtuan123 <lqtuan0102@gmail.com>
2024-10-23 18:20:38 +0700

    update notification

--- resources/views/backend/users/edit.blade copy.php
+++ resources/views/backend/users/edit.blade copy.php
@@ -117,19 +117,6 @@
                             </select>
                         </div>
                     </div>
-                    <div class="mt-3">
-                        
-                            @if($errors->any())
-                             
-                            <div class="alert alert-danger">
-                                <ul>
-                                        @foreach ($errors->all() as $error)
-                                            <li>    {{$error}} </li>
-                                        @endforeach
-                                </ul>
-                            </div>
-                            @endif
-                    </div>
                     <div class="text-right mt-5">
                         <button type="submit" class="btn btn-primary w-24">Lưu</button>
                     </div>
