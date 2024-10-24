dd6ccc6ad459e0c28b82d625e84b2596dd9a3f6c
lqtuan123 <lqtuan0102@gmail.com>
2024-10-24 22:14:15 +0700

    update notification

--- resources/views/backend/layouts/notification.blade.php
+++ resources/views/backend/layouts/notification.blade.php
@@ -21,5 +21,16 @@
 </div>
    
 @endif
+</div>
 
+<div class="mt-3">
+    @if($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
 </div>
