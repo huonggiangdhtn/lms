7b84d68c0bd9325ae8c570de1d34287fd160561c
lqtuan123 <lqtuan0102@gmail.com>
2024-12-04 21:40:16 +0700

    #70 update_event

--- resources/views/backend/layouts/sidebar.blade.php
+++ resources/views/backend/layouts/sidebar.blade.php
@@ -321,6 +321,31 @@
     </li>
        <!-- eventtype -->
 
+{{-- <li>
+    <a href="javascript:;" class="side-menu {{($active_menu=='event_list'|| $active_menu=='event_add')?'side-menu--active':''}}">
+        <div class="side-menu__icon"> <i data-lucide="calendar"></i> </div> <!-- Thay đổi icon ở đây -->
+        <div class="side-menu__title">
+            Sự kiện
+            <div class="side-menu__sub-icon transform"> <i data-lucide="chevron-down"></i> </div>
+        </div>
+    </a>
+    <ul class="{{($active_menu=='event_list'|| $active_menu=='event_add')?'side-menu__sub-open':''}}">
+        <li>
+            <a href="{{route('admin.eventtype.index')}}" class="side-menu {{$active_menu=='event_list'?'side-menu--active':''}}">
+                <div class="side-menu__icon"> <i data-lucide="list"></i> </div>
+                <div class="side-menu__title">Danh sách sự kiện</div>
+            </a>
+        </li>
+        <li>
+            <a href="{{route('admin.eventtype.create')}}" class="side-menu {{$active_menu=='event_add'?'side-menu--active':''}}">
+                <div class="side-menu__icon"> <i data-lucide="plus"></i> </div>
+                <div class="side-menu__title">Thêm Sự kiện</div>
+            </a>
+        </li>
+    </ul>
+
+</li> --}}
+
 <li>
     <a href="javascript:;" class="side-menu side-menu{{($active_menu=='eventtype_list' || $active_menu=='eventtype_add' || $active_menu=='eventtype_edit') ? '--active' : ''}}">
         <div class="side-menu__icon"> <i data-lucide="calendar"></i> </div>
@@ -344,6 +369,7 @@
         </li>
     </ul>
 </li>
+
 <li>
     <a href="javascript:;" class="side-menu {{($active_menu=='major_list'|| $active_menu=='major_add')?'side-menu--active':''}}">
         <div class="side-menu__icon"> 
